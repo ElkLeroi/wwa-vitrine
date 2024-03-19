6d1e3807052271b4eeb662d8b46a9ecb30c0963c
ElkLeroi <84155709+ElkLeroi@users.noreply.github.com>
2024-03-19 16:48:02 +0000

    menuR

--- client/static/client/assets/js/home.js
+++ client/static/client/assets/js/home.js
@@ -13,3 +13,44 @@ function isValidNumericInput(input) {
     return numericRegex.test(input);
 }
 
+// 
+
+
+
+        function Menu(e){
+            let list = document.querySelector('ul')
+            const menuR = document.getElementById('menuR');
+
+            e.name === 'menu' ? (
+                e.name = "close", list.classList.add('top-[80px]'), 
+                list.classList.add('opacity-100'),
+                console.log("Done"),
+                document.getElementById('menuR').style.display = 'flex',
+                document.getElementById('sticky').style.filter = 'blur(3px)',
+                console.log("menuR display property value:", menuR.style.display),
+                OutsideClose()
+
+            ) : (e.name = "menu", list.classList.remove('top-[80px]'), 
+                list.classList.remove('opacity-100'),
+                document.getElementById('menuR').style.display = 'none',
+                document.getElementById('sticky').style.filter = 'blur(0px)',
+                console.log("menuR display property value:", menuR.style.display)
+            )
+        }
+        
+        function OutsideClose() {
+            console.log("Here we go !!!");
+            if (menuR.style.display === 'flex') {
+                document.addEventListener('click', function(event) {
+                const menuR = document.getElementById('menuR');
+                    if (!menuR.contains(event.target)) {
+                        document.getElementById('menuR').style.display = 'none';
+                        document.getElementById('sticky').style.filter = 'blur(0px)';
+                    }
+                });
+            }
+        }
+        
+        
+    </script>
+
