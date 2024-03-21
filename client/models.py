d7e3f844aca4defa7e3386b5542ce0f306c5f163
ElkLeroi <84155709+ElkLeroi@users.noreply.github.com>
2024-03-21 16:55:35 +0000

    Test 2 ok

--- client/models.py
+++ client/models.py
@@ -11,7 +11,7 @@ def prospect_pic(instance, filename):
 class Prospect(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=100)
-    telephone = models.PositiveBigIntegerField(unique=True)
+    telephone = models.PositiveBigIntegerField(unique=True, null=True, blank=True)
     mail = models.CharField(max_length=100, unique=True)
     picture = models.ImageField(upload_to=prospect_pic)
     frequency = models.PositiveBigIntegerField(default=0)
